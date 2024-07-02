27486cc2786899c8e79c18c382d0bf31c63649e2
Ricardo Araya <ricardo.araya@conytec.com>
2024-07-02 17:35:40 -0400

    static fix

--- avd/settings.py
+++ avd/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 """
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
